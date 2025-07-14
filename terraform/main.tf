8dad0fc0514b2cede2b2cc3954e8491bdc8338b4
Yassine BERRADA <yassine.berrada2020@gmail.com>
2025-07-14 13:49:06 +0100

    modifications de main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "abc" {
   ami           = "ami-053b0d53c279acc90"  # Ubuntu 22.04 LTS dans us-east-1
   instance_type = "m5.2xlarge"             # 8 vCPU, 32 Go RAM
+  key_name      = aws_key_pair.terraform_key.key_name  
 
   tags = {
     Name = "abc"
@@ -15,4 +16,15 @@ resource "aws_instance" "abc" {
     volume_type = "gp2"
   }
 }
+
+resource "aws_key_pair" "terraform_key" {
+  key_name   = "terraform-key"
+  public_key = file("/home/ubuntu/.ssh/terraform-key.pub")  # ou chemin absolu
+}
+
+
+output "instance_ip" {
+  value = aws_instance.abc.public_ip
+}
+
 
